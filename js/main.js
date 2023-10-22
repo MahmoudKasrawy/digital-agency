9d52855723bbb8138df822e0af5f5c8f6e55a58f
mahmoud medhat <108630397+MahmoudKasrawy@users.noreply.github.com>
2023-10-22 22:34:48 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -56,26 +56,26 @@
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        dots: false,
-        loop: true,
-        nav: true,
-        navText: [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ],
-        responsive: {
-            0: {
-                items: 1
-            },
-            992: {
-                items: 2
-            }
-        }
-    });
+    // $(".testimonial-carousel").owlCarousel({
+    //     autoplay: true,
+    //     smartSpeed: 1000,
+    //     margin: 25,
+    //     dots: false,
+    //     loop: true,
+    //     nav: true,
+    //     navText: [
+    //         '<i class="bi bi-chevron-left"></i>',
+    //         '<i class="bi bi-chevron-right"></i>'
+    //     ],
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         992: {
+    //             items: 2
+    //         }
+    //     }
+    // });
 
 
     // Portfolio isotope and filter
@@ -102,7 +102,7 @@
 //   owl-carousel
 $(' .owl-carousel').owlCarousel({
     loop:true,
-    margin:15,
+    margin:25,
     responsiveClass:true,
    navText: [
                 '<i class="bi bi-chevron-left"></i>',
